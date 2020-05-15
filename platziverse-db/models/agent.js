1ed1d18082601ed068f227168d6455f5b2196732
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-14 23:30:47 -0500

    se agrego el codigo al modelo de metric

--- platziverse-db/models/agent.js
+++ platziverse-db/models/agent.js
@@ -3,30 +3,30 @@
 const Sequelize = require('sequelize')
 const setupDatabase = require('../lib/db')
 
-module.exports = function setupAgentModel(config) {
-    const sequelize = setupDatabase(config)
+module.exports = function setupAgentModel (config) {
+  const sequelize = setupDatabase(config)
 
-    return sequelize.define('agent',{
-        uuid:{
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        username:{
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        name:{
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        hostname:{
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        pid:{
-            type: Sequelize.INTEGER,
-            allowNull: false,
-            defaultValue: false
-        }
-    })
-  }
+  return sequelize.define('agent', {
+    uuid: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    hostname: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    pid: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: false
+    }
+  })
+}
