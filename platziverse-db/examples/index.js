45217c5f675a1bc6642eba3b42dfada612e9dc3a
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-16 13:10:50 -0500

    se agrego examaples para correr pruebas de ejemplo  directas con la db

---
+++ platziverse-db/examples/index.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const db = require('../')
+
+async function run(){
+    const config = {
+        database: process.env.DB_NAME || 'platziverse',
+        username: process.env.DB_USER || 'platzi',
+        password: process.env.DB_PASS || 'platzi',
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'postgres'
+    }
+
+    const { Agent, Metric } = await db(config).catch(handleFatalError)
+
+    const agent = await Agent.createOrUpdate({
+        uuid: 'yyy',
+        name: 'test',
+        username:'test',
+        hostname: 'test',
+        pid: 1,
+        connected: true
+    }).catch(handleFatalError)
+
+    console.log('--agent insert--')
+    console.log(agent)
+
+    const agents = await Agent.findAll().catch(handleFatalError)
+    console.log('-- AGENTS QUERY -- ')
+    console.table(agents)
+
+
+    const metrics = await Metric.findByAgentUuid(agent.uuid).catch(handleFatalError)
+    console.log('--metrics--')
+    console.table(metrics)
+  
+    const metric = await Metric.create(agent.uuid, {
+      type: 'memory',
+      value: '300'
+    }).catch(handleFatalError)
+  
+    console.log('--metric--')
+    console.table(metric)
+  
+    const metricsByType = await Metric.findByTypeAgentUuid('memory', agent.uuid).catch(handleFatalError)
+    console.log(' -- METRICS QUERY FIND BY AGENT --')
+    console.table(metricsByType)
+}
+
+function handleFatalError(err){
+    console.error(err.message)
+    console.error(err.stack)
+    process.exit(1)
+}
+run()
