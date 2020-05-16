ab5460264d3bfc1d36a870be7ef60e7d012f4e91
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-16 14:00:48 -0500

    se resolvio el reto de pasar parametros en el comando de setup. clase 26

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -7,16 +7,19 @@ const db = require('./')
 
 const prompt = inquirer.createPromptModule()
 
+const args = process.argv.slice();
 async function setup () {
-  const answer = await prompt([
-    {
-      type: 'confirm',
-      name: 'setup',
-      message: 'This will destroy your database? are you sure?'
+  if(!args.includes( '-y') && !args.includes( '--yes' )){
+    const answer = await prompt([
+      {
+        type: 'confirm',
+        name: 'setup',
+        message: 'This will destroy your database? are you sure?'
+      }
+    ])
+    if (!answer.setup) {
+      return console.log('Nothing happened :)')
     }
-  ])
-  if (!answer.setup) {
-    return console.log('Nothing happened :)')
   }
 
   const config = {
