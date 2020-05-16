1945d5ba9a5a7369e29caa983c82874368888102
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-15 19:40:04 -0500

    se agregaron las dependencias de inquirer y chalk para manejo del archivo setup en terminal

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -1,16 +1,31 @@
 'use strict'
 
 const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 const db = require('./')
 
+const prompt = inquirer.createPromptModule()
+
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database? are you sure?'
+    }
+  ])
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
+
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'platzi',
     password: process.env.DB_PASS || 'platzi',
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
-    logging: s => debug(s),
+    logging: (s) => debug(s),
     setup: true
   }
 
@@ -21,7 +36,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[FATAL ERROR!!]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
