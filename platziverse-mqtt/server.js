50780daf22df88a7d5ec8de158848e8c3b1db490
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-16 19:28:26 -0500

    se agrego el modulo mqtt para manejo de mensajes con IoT

---
+++ platziverse-mqtt/server.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const debug = require('debug')('platziverse:mqtt')
+const mosca = require('mosca')
+const redis = require('redis')
+const chalk = require('chalk')
+
+const backend = {
+    type: 'redis',
+    redis,
+    return_buffers: true// con esto la información viene binaria y la podra procesar mucho mas facil!
+}
+
+const settings = {
+    port: 1883,
+    backend
+}
+
+const server = new mosca.Server(settings)
+
+server.on('clientConnected', client => {
+    debug(`Client Connected: ${client.id}`)
+})
+server.on('clientDisconnected', client => {
+    debug(`Client Disconnected: ${client.id}`)
+}) 
+
+server.on('published', (packet, client) => {
+    debug(`Received: ${packet.topic}`)
+    debug(`Payload: ${packet.payload}`)
+})
+
+server.on('ready', ()=>{
+    console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
+} )
+
+server.on('error', handleFatalError)
+
+function handleFatalError(err){
+    console.error(`${chalk.red('[FATAL ERROR]')} ${err.message}`)
+    console.error(err.stack)
+    process.exit(1)
+}
+
+process.on('uncaughtException', handleFatalError)
+process.on('unhandledRejection', handleFatalError)
