b9482cb41fef70e4c2a5ee13a8e9418dd27ffa0f
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-17 00:56:10 -0500

    se agrego conexion , uuid y parsePayload

--- platziverse-agent/README.md
+++ platziverse-agent/README.md
@@ -11,6 +11,14 @@ const agent = new PlatziverseAgent({
 
 agent.connect()
 
+//this agent only
+agent.on('connected')
+agent.on('disconnected')
+agent.on('message')
+
+
+agent.on('agent/connected')
+agent.on('agent/disconnected')
 agent.on('agent/message', payload => {
   console.log(payload)
 })
--- platziverse-agent/index.js
+++ platziverse-agent/index.js
@@ -1,21 +1,68 @@
 'use strict'
+
+const debug = require('debug')('platziverse:agent')
+const mqtt = require('mqtt')
+const defaults = require('defaults')
 const EventEmitter = require('events')
+const uuid = require('uuid')
+const {parsePayload} = require('platziverse-utils')
+const options = {
+    name: 'untitled',
+    username: 'platzi',
+    interval: 5000,
+    mqtt: {
+        host: 'mqtt://localhost'
+    }
+
+}
 
 class PlatziverseAgent extends EventEmitter{
     constructor(opts){
         super()
-        this._options = opts
+        this._options = defaults(opts, options)
+        this._started = false
         this._started = false
         this._timer = null  
+        this._client = null
+        this._agentId = null
     }
     connect(){
         if(!this._started){
-            this._started= true
-            this.emit('connected')
             const opts = this._options 
-            this._timer = ServiceWorkerContainer(() => {
-                this.emit(('agent/message','This is a message'))
-            }, opts.interval)
+            this._client = mtqq.connect(opts.mqtt.host)
+            this._started= true
+
+            this._client.subscribe('agent/message')
+            this._client.subscribe('agent/connected')
+            this._client.subscribe('agent/disconnected')
+
+            this._client.on('connect',() => {
+                this._agentId = uuid.v4()
+                
+                this.emit('connected', this._agentId) 
+
+                this._timer = ServiceWorkerContainer(() => {
+                    this.emit(('agent/message','This is a message'))
+                }, opts.interval)
+            })
+            this._client.on('message',(topic, payload) => {
+                payload = parsePayload(payload)
+
+                let broadcast = false
+                switch (topic) {
+                    case 'agent/connected':
+                    case 'agent/disconnected':
+                    case 'agent/message':
+                        broadcast = payload && payload.agent && payload.agent.uuid !== this._agentId
+                        break;
+                }
+
+                if(broadcast)
+                    this.emit(topic, payload)
+
+            })
+
+            this._client.on('error',() => this.disconnect())
         }
     }
     disconnect(){
