6ca071c65538474ed7ce999bfc42af9cb556c6df
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-16 19:48:53 -0500

    se agrego como dependencia nuestro platziverse-db para usarlo en nuestro server de mensajes

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -4,42 +4,57 @@ const debug = require('debug')('platziverse:mqtt')
 const mosca = require('mosca')
 const redis = require('redis')
 const chalk = require('chalk')
+const db = require('platziverse-db')
 
 const backend = {
-    type: 'redis',
-    redis,
-    return_buffers: true// con esto la información viene binaria y la podra procesar mucho mas facil!
+  type: 'redis',
+  redis,
+  return_buffers: true // con esto la información viene binaria y la podra procesar mucho mas facil!
 }
 
 const settings = {
-    port: 1883,
-    backend
+  port: 1883,
+  backend
+}
+const config = {
+  database: process.env.DB_NAME || 'platziverse',
+  username: process.env.DB_USER || 'platzi',
+  password: process.env.DB_PASS || 'platzi',
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+  logging: (s) => debug(s)
 }
 
 const server = new mosca.Server(settings)
 
-server.on('clientConnected', client => {
-    debug(`Client Connected: ${client.id}`)
+let Agent, Metric
+
+server.on('clientConnected', (client) => {
+  debug(`Client Connected: ${client.id}`)
 })
-server.on('clientDisconnected', client => {
-    debug(`Client Disconnected: ${client.id}`)
-}) 
+server.on('clientDisconnected', (client) => {
+  debug(`Client Disconnected: ${client.id}`)
+})
 
 server.on('published', (packet, client) => {
-    debug(`Received: ${packet.topic}`)
-    debug(`Payload: ${packet.payload}`)
+  debug(`Received: ${packet.topic}`)
+  debug(`Payload: ${packet.payload}`)
 })
 
-server.on('ready', ()=>{
-    console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
-} )
+server.on('ready', async () => {
+  const services = await db(config).catch(handleFatalError)
+  Agent = services.Agent
+  Metric = services.Metric
+
+  console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
+})
 
 server.on('error', handleFatalError)
 
-function handleFatalError(err){
-    console.error(`${chalk.red('[FATAL ERROR]')} ${err.message}`)
-    console.error(err.stack)
-    process.exit(1)
+function handleFatalError (err) {
+  console.error(`${chalk.red('[FATAL ERROR]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
 }
 
 process.on('uncaughtException', handleFatalError)
