8f519f512b64fa897ddcec6ac7553f32e6b359ad
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-05-16 23:52:12 -0500

    se agrego el modulo de platziverse-agent

---
+++ platziverse-agent/index.js
@@ -0,0 +1,32 @@
+'use strict'
+const EventEmitter = require('events')
+
+class PlatziverseAgent extends EventEmitter{
+    constructor(opts){
+        super()
+        this._options = opts
+        this._started = false
+        this._timer = null  
+    }
+    connect(){
+        if(!this._started){
+            this._started= true
+            this.emit('connected')
+            const opts = this._options 
+            this._timer = ServiceWorkerContainer(() => {
+                this.emit(('agent/message','This is a message'))
+            }, opts.interval)
+        }
+    }
+    disconnect(){
+        if(this._started){
+            clearInterval(this._timer)
+            this._started = false
+            this.emit('disconnected')
+        }
+    }
+}
+
+
+module.exports = PlatziverseAgent
+º
